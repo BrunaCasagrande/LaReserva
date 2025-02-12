698254ca8cd1ce36bbf700d551876f04ba2996f8
BrunaCasagrande <brunaczaramela@gmail.com>
2025-02-12 20:55:38 -0300

    commit de criacao do projeto

---
+++ src/main/java/com/api/lareserva/LareservaApplication.java
@@ -0,0 +1,13 @@
+package com.api.lareserva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LareservaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LareservaApplication.class, args);
+	}
+
+}
