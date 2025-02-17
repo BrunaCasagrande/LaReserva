242f05cf5667aecd8ae7ec5a894e920c15748346
BrunaCasagrande <brunaczaramela@gmail.com>
2025-02-17 20:23:50 -0300

    add repositories

---
+++ src/main/java/com/api/lareserva/infrastructure/persistence/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.api.lareserva.infrastructure.persistence.repository;
+
+import com.api.lareserva.infrastructure.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+}
