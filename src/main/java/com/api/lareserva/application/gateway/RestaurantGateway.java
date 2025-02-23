fccd1f4a2ccc84540dcf0198ea9b71d8965dc0ad
BrunaCasagrande <brunaczaramela@gmail.com>
2025-02-23 16:11:25 -0300

    Start implementation

--- src/main/java/com/api/lareserva/application/gateway/RestaurantGateway.java
+++ src/main/java/com/api/lareserva/application/gateway/RestaurantGateway.java
@@ -1,4 +1,3 @@
 package com.api.lareserva.application.gateway;
 
-public interface RestaurantGateway {
-}
+public interface RestaurantGateway {}
--- src/main/java/com/api/lareserva/application/gateway/UserGateway.java
+++ src/main/java/com/api/lareserva/application/gateway/UserGateway.java
@@ -1,8 +1,15 @@
 package com.api.lareserva.application.gateway;
 
 import com.api.lareserva.application.domain.User;
+import java.util.Optional;
 
 public interface UserGateway {
 
-    User save(final User user);
+  User save(final User user);
+
+  Optional<User> findById(final int id);
+
+  User update(final User user);
+
+  void deleteById(final int id);
 }
